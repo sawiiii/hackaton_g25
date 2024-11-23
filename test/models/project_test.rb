fc6f4086b440fb88a01d28613e81169d1c6784c1
Vicente Sagüez Laage <vsaguezlaage@gmail.com>
2024-11-23 12:42:17 -0300

    arreglo de cambios

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,12 +4,12 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should save project with valid attributes" do
+  test "should fail save project with invalid attributes" do
     project = Project.new(name: "New Project", description: "Project description")
     assert_not project.save, "Failed to save a valid project"
   end
 
-  test "should not save project without a name" do
+  test "should save project with right attributes" do
     person = Person.new(name: "John Doe", auth0_id: "auth0_123456")
     project = Project.new(name: "New Project", description: "Project description", owner: person)
     assert project.save, "Saved the project without a name"
