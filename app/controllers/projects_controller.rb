563be05b48137e98c6c7af418263c3beab7e6424
Vicente Sagüez Laage <vsaguezlaage@gmail.com>
2024-11-23 15:49:22 -0300

    validacion para creacion de proyecto

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   before_action :authorize
-  #before_action :validate_current_user
+  before_action :validate_current_user, only: :create
   before_action :set_project, only: %i[ show update destroy ]
 
   # GET /projects
