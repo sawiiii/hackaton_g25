048b76be1b726c68e8051cc242a013f5b707c34e
Vicente Sagüez Laage <vsaguezlaage@gmail.com>
2024-11-23 15:34:41 -0300

    develop

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,9 +8,9 @@ class PeopleController < ApplicationController
     if params[:sub].present?
       person = Person.find_by(auth0_id: params[:sub])
       if person
-        render json: [person]
+        render json: [person], status: :success
       else
-        render json: [], status: :not_found
+        render json: [], status: :success
       end
     else
       @people = Person.all
